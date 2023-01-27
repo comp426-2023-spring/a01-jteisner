44fdb224751b3f005bcbef7ad692d09c75ea8b5c
Jacob Eisner <jteisner@email.unc.edu>
2023-01-26 19:56:29 -0500

    try 8

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ fs.readFile("./public/index.html", "utf-8", (err,data) =>{
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
-    res.end(retdata);
+    res.end(data);
 });
 // Start the `server` const listening on the port defined by argument in your `port` const. 
 // Put the exact message `Server listening on port ${port}` on the console log. 
